064a47a8a727b21ab0e3efb10d1b450f4e855a40
Jeremy-Dane <jeremy-dane@ubuntu.(none)>
2014-04-29 12:43:32 -0400

    Progress Project 3... :S

--- static/cards.js
+++ static/cards.js
@@ -132,11 +132,11 @@ window.onload=function()
 		
 		if (mode==1)
 		{
-		 	players=solo();
+		 	players=solo(1);
 		}
 		else
 		{
-			alert("2 players");
+			//alert("2 players");
 
 		 	if (mode==2)
 			{
@@ -148,24 +148,26 @@ window.onload=function()
 		 	}
 		
 		}
-			
-		$('#start').hide();
+		return mode;	
+		//$('#start').hide();
 		
-		$('#deck').show();
-		document.getElementById('sign').innerHTML =player;
-		$('#start').show();
-		startGame();
+		//$('#deck').show();
+		//document.getElementById('sign').innerHTML =player;
+		//$('#start').show();
+		//startGame();
 	}
 
 	
 
 	var gameScreen = function()
 	{
-		var go= document.getElementById('go');		
-		go.parentNode.removeChild(go);
+		//var go= document.getElementById('go');		
+		//go.parentNode.removeChild(go);
+		$('#go').hide();
+		$('#heading').hide();
 
-		var head= document.getElementById('heading');
-		head.parentNode.removeChild(heading);
+		//var head= document.getElementById('heading');
+		//head.parentNode.removeChild(heading);
 		
 		var start= document.getElementById('start');
 		start.style.cssFloat="right";
@@ -216,16 +218,20 @@ window.onload=function()
 		p2 = window.prompt('Player2:');
 		localStorage["player2"]= p2;
 
-		players=[p1,p2];
+		players=[p1,p2].toString();
 
+		console.log(players);
 		//return 	players	
 		$('#start').hide();
 		
 		$('#deck').show();
-		document.getElementById('sign').innerHTML =player;
-		startGame(2);
+		document.getElementById('sign').innerHTML =p1+" vs "+p2;
+		//for (i=0; i<2; i++)
+
+		startGame(2,type);
+		
 		$('#start').show();
-		return type;
+		//return type;
 	}
 	var solo=function()
 	{
@@ -238,12 +244,12 @@ window.onload=function()
 		
 		$('#deck').show();
 		document.getElementById('sign').innerHTML =player;
-		startGame(1);
+		
+		startGame(1, 0);
+
 		$('#start').show();	
-		return 0;	
+		//return 0;	
 	}
-
-
 	
 	function gameMode()
 	{
@@ -286,24 +292,72 @@ window.onload=function()
 		container.appendChild(label3);
 	}
 	
-	var startGame = function(type)
+	var startGame = function(num, type)
 	{	
+		$('#mode').hide();
+		resume();
+		
+		if (num=1)
+		{	
+			gameScreen(num);
 			
-		gameScreen();
-		
-		arrangeCards();
-		$('#save').show();
-		$('#cardsPic').hide();
-		
-		var play=true;
-		var clk=0;
-		var stk=[];
+			arrangeCards();
+			$('#save').show();
+			$('#cardsPic').hide();
+			
+			var play=true;
+			var clk=0;
+			var stk=[];
 
-		$('#start').hide();
-				
-		cardClick(clk, stk);
+			$('#start').hide();
+					
+			cardClick(clk, stk);
+		}
+		if (num=2)
+		{	
+			gameScreen(num);
+			
+			arrangeCards();
+			$('#save').show();
+			$('#cardsPic').hide();
+			
+			var play=true;
+			var clk=0;
+			var stk2=[1][0];			
+
+			$('#start').hide();
+					
+			cardClick2(clk, stk2);
+		}
+		// if (num=2)
+		// {	
+		// 	gameScreen(num);
+			
+		// 	arrangeCards();
+		// 	$('#save').show();
+		// 	$('#cardsPic').hide();
+			
+		// 	var play=true;
+		// 	var clk=0;
+		// 	var stk1=[];
+		// 	var stk2=[];
+
+		// 	$('#start').hide();
+					
+		// 	cardClick(clk, stk1, stk2);
+		// }
 
 		
+	}
+	var resume = function()
+	{
+		for(var i in window.localStorage)
+		{
+   			val = localStorage.getItem(i); 
+   			value = val.split(","); //splitting string inside array to get name
+   			name[i] = value[1]; // getting name from split string
+   			console.log(i);
+		}
 	}
         
 	var cardClick = function(clk, stk)
@@ -326,7 +380,7 @@ window.onload=function()
 			
 				document.getElementById('turnValue').innerHTML =turns;
 			
-	    			card.addClass('flipped');
+	    		card.addClass('flipped');
 				playing(card, stk);	
 			}
 			else
@@ -346,6 +400,69 @@ window.onload=function()
 		
 		
 	}
+	var cardClick2 = function(clk, stk2)
+	{	
+		$('.card').click(function()
+		{
+			p1=localStorage["player1"]
+			p2=localStorage["player2"]
+			for (i=1; i<=2;i++)
+			{
+			
+				var flip=document.getElementById('flipsound');
+				flip.play();
+			
+				clk++;			
+				card=$(this);
+				
+				turns[i]= Math.floor(clk/(2*i));
+
+				console.log(turns[i]);
+
+				localStorage["turns"+i]= turns[i] ;
+				
+				document.getElementById('turnValue').innerHTML =[p1,turns[1]];
+				document.getElementById('turnValue2').innerHTML =[p2,turns[2]];
+				
+		    	card.addClass('flipped');
+				playing(card, stk2);	
+			}
+
+		});	
+		
+	}
+
+	// var cardClick = function(clk, stk1, stk2)
+	// {	
+	// 	$('.card').click(function()
+	// 	{
+	// 		p1=localStorage["player1"]
+	// 		p2=localStorage["player2"]
+	// 		for (i=1; i<=2;i++)
+	// 		{
+			
+	// 			var flip=document.getElementById('flipsound');
+	// 			flip.play();
+			
+	// 			clk++;			
+	// 			card=$(this);
+				
+	// 			turns[i]= Math.floor(clk/(2*i));
+
+	// 			console.log(turns[i]);
+
+	// 			localStorage["turns"+i]= turns[i] ;
+				
+	// 			document.getElementById('turnValue').innerHTML =[p1,turns[1]];
+	// 			document.getElementById('turnValue2').innerHTML =[p2,turns[2]];
+				
+	// 	    	card.addClass('flipped');
+	// 			playing(card, stk1, stk2);	
+	// 		}
+
+	// 	});	
+		
+	// }
 
 	var  playing = function(card, stk)
 	{
@@ -399,6 +516,109 @@ window.onload=function()
 		}		
 	}
 
+	// var  playing = function(card, stk1, stk2)
+	// {
+	// 	for (i=1; i<=2;i++)
+	// 	{
+	// 		type=card[0].textContent;
+	// 		//id=card[0].id;
+	// 		e=card;
+	// 		stk1.push({element:e,type:type});
+			
+	// 		faces=[];
+	// 		if (stk1.length>1)
+	// 		{
+	// 			x=stk1[0].type;
+	// 			y=stk1[1].type;
+	// 			faces.push(x);
+	// 			faces.push(y);
+				
+
+	// 			//a=stk[0].id;
+	// 			//b=stk[1].id;
+				
+	// 			if (paired(faces))
+	// 			{					
+										
+	// 				//document.getElementById(a).addClass("matched");
+	// 				//document.getElementById(b).addClass("matched");
+
+	// 				stk1[1].element.addClass("matched");
+	// 				stk1[0].element.addClass("matched");
+
+	// 				var same=document.getElementById('match');
+	// 				same.play();
+					
+	// 				faces.pop();
+	// 				faces.pop();	
+
+	// 			}
+	// 			else
+	// 			{	
+	// 				//document.getElementById("card1").addClass("flip");
+	// 				//document.getElementById("card2").addClass("flip");
+	// 				for (i=0;i<2;i++)
+	// 				{	
+	// 					stk1[i].element.removeClass("flipped");
+						
+	// 				}
+	// 				stk1.pop();
+	// 				stk1.pop();
+	// 			}
+
+	// 		}
+	// 	}
+	// 	for (i=1; i<=2;i++)
+	// 	{
+	// 		type=card[0].textContent;
+	// 		//id=card[0].id;
+	// 		e=card;
+	// 		stk2.push({element:e,type:type});
+			
+	// 		faces=[];
+	// 		if (stk2.length>1)
+	// 		{
+	// 			x=stk2[0].type;
+	// 			y=stk2[1].type;
+	// 			faces.push(x);
+	// 			faces.push(y);
+				
+
+	// 			//a=stk[0].id;
+	// 			//b=stk[1].id;
+				
+	// 			if (paired(faces))
+	// 			{					
+										
+	// 				//document.getElementById(a).addClass("matched");
+	// 				//document.getElementById(b).addClass("matched");
+
+	// 				stk2[1].element.addClass("matched");
+	// 				stk2[0].element.addClass("matched");
+
+	// 				var same=document.getElementById('match');
+	// 				same.play();
+					
+	// 				faces.pop();
+	// 				faces.pop();	
+
+	// 			}
+	// 			else
+	// 			{	
+	// 				//document.getElementById("card1").addClass("flip");
+	// 				//document.getElementById("card2").addClass("flip");
+	// 				for (i=0;i<2;i++)
+	// 				{	
+	// 					stk2[i].element.removeClass("flipped");
+						
+	// 				}
+	// 				stk2.pop();
+	// 				stk2.pop();
+	// 			}
+
+	// 		}
+	// 	}		
+	// }
 
 	var paired = function(stk)
 	{
@@ -416,8 +636,25 @@ window.onload=function()
 	var saveGame = function()
 	{
 		alert("Saving Game");
-		var deck = $("deck").childNodes;
-		//localStorage["game"]= deck.toString() ;
+		//var deck = $("deck").childNodes;
+		user1=localStorage["player1"];
+		user2=localStorage["player2"];
+		turns= localStorage["turns"];
+		game= $('#deck').html();
+		console.log(game);
+		//console.log($('#deck'));
+		//console.log($('#deck')[0].html());
+		//localStorage["game"]= JSON.stringify(html());
+		//localStorage.setItem('game', game);
+
+		//console.log($("#deck").html()));
+		//console.log(JSON.parse(game));
+
+		//console.log($('#deck').getElementsByClassName("card"));
+		//console.log($('#deck').getElementsByClassName("cards"));
+
+
+		localStorage[user1+""]= JSON.stringify({name: [user1,user2], turns: turns, game: game});
 	}
 
 	var deck = shuffle(40);
@@ -428,19 +665,37 @@ window.onload=function()
 	
 	//document.getElementById('multi').onclick=prelim;
 	//document.getElementById('start').onclick=prelim;
-
+	
 	$('#start').click(function()
 	{
-		$('#multi').hide();
-		solo();
+		$('#mode').hide();
+		gameMode();
+		//document.getElementById('myRadioId1').checked 
+		mode= prelim();
+		$('#multi').hide()
+		if (mode=0)
+		{
+			$('#mode').hide();
+			solo(0);
+		}
 	});
 	$('#multi').click(function()
 	{
 		
 		//$('#start').hide();
 		gameMode();
+		mode= prelim();
 		
-		prelim();
+		//mode= prelim();
+		$('#multi').hide();
+
+		$('#start').click(function()
+		{
+			$('#mode').hide();
+			duo(mode);
+		});
+		
+		
 	});
 	//event.preventDefault();
 	document.getElementById('save').onclick=saveGame;
